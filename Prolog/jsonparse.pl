0ad7b1b36511dc59a600f2ae7f17f7b7cfd92c0e
Alessandro-Crespi <alessandro.crespi.2002@gmail.com>
2022-12-29 15:20:41 +0100

    jsonaccess implemented 90/100

--- Prolog/jsonparse.pl
+++ Prolog/jsonparse.pl
@@ -1,6 +1,13 @@
+
+%%% JSONPARSE/2 IMPLEMENTATION
+
+jsonparse({}, jsonobj([])) :- !.
+jsonparse([], jsonarray([])) :- !.
+
 %% entrypoint atom-to-string
 jsonparse(AtomJSON, ParsedObject) :-
     atom(AtomJSON), !,
+    not(jsonatom(AtomJSON)),
     atom_string(AtomJSON, StringJSON),
     jsonparse(StringJSON, ParsedObject).
 
@@ -23,10 +30,13 @@ boolean(true).
 boolean(false).
 nullable(null).
 
-jsonprimitive(P, P) :- boolean(P), !.
-jsonprimitive(P, P) :- string(P), !.
-jsonprimitive(P, P) :- number(P), !.
-jsonprimitive(P, P) :- nullable(P), !.
+jsonatom(P) :- boolean(P), !.
+jsonatom(P) :- string(P), !.
+jsonatom(P) :- number(P), !.
+jsonatom(P) :- nullable(P), !.
+
+jsonprimitive(P, P) :- 
+    jsonatom(P), !.
 jsonprimitive(P, ParsedP) :- 
     jsonparse(P, ParsedP), !.
 
@@ -47,13 +57,54 @@ jsonobj({Object}, [ParsedMember | ParsedMoreMember]) :-
 jsonobj({Pair}, [ParsedPair]) :-
     jsonpair(Pair, ParsedPair).
 
-
-
 /*  DEMOS:
 jsonparse({"key1":123, "key2":"pippo", "key3":true}, X).
- X = jsonobj(["key1":123, "key2":"pippo", "key3":true]).
+    X = jsonobj([("key1", 123), ("key2", "pippo"), ("key3", true)]).
 
 jsonparse({"key1":{"key1.1":false}, "key2":"pippo", "key3":["a", "b", 3, 4]}, X).
 
+------------
+set 'test' var : 
+    jsonparse({"key1":123, "key2":["ciao", "abc", 123, 621, {"code":"value", "foo":999}, null], "key3":true}, X), nb_setval(test, X).
 
+UTILS:
+print all output : (BUG: string delimiter ' " ' will no more printed')
+    set_prolog_flag(answer_write_options,[max_depth(0)]).
 */
+
+
+%%% JSONACCESS/3 IMPLEMENTATION
+jsonparsable(jsonobj(JSON)) :-
+    jsonobj(_,JSON), !.
+jsonparsable(jsonarray(JSON)) :-
+    jsonarray(_,JSON), !.
+jsonparsable(JSON) :-
+    jsonatom(JSON), !.
+
+% jsonaccess(jsonarray(_), [], _) :- !, false.
+jsonaccess(JSON, [], JSON) :- 
+    jsonparsable(JSON), !.
+
+jsonaccess(jsonobj(Object), [Key | Pattern], Result) :-
+    string(Key),
+    memberchk((Key, Content), Object),
+    jsonaccess(Content, Pattern, Result),
+    !.
+
+%% handle arrays
+jsonaccess(jsonarray(Array), [Index | Pattern], Result) :-
+    number(Index),
+    nth0(Index, Array, Content),
+    jsonaccess(Content, Pattern, Result),
+    !.
+
+
+t(V) :- nb_getval(test,V).
+
+
+
+%%% JSONREAD/2
+%% jsonread(-JSON, +Filename)
+jsonread(JSON, FileName) :-
+    nonvar(JSON),
+    var(FileName),
